9f70707378d400d53ad1f096ab61536b75d776d7
nashri <nashriaziz65@gmail.com>
2021-11-12 18:24:48 +0700

    Nashri12

--- src/App.js
+++ src/App.js
@@ -2,37 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="agen_login_page">
-      <div className='Body'>
-      <div className='Header'> 
-        <header>BANK LOGO</header>
-      </div>
-      
-      <div className='login_form'>
-        <form action="#">
-          <div className='error_txt'>Agent not found</div>
-          <div className='email_details'>
+  <body>
+    <header>BANK NAME</header>
+    <div className='warper'>
+      <section className='form signup'>
+        <form action='#'>
+          <div className='error-txt'>Agent not found!</div>
+          <div className='field input'>
             <label>Email</label>
-            <input type='text' placeholder='Email'></input>
+            <input type='text'></input>
           </div>
-
-          <div className='password_details'>
+          <div className='field input'>
             <label>Password</label>
-            <input type='password' placeholder='password'></input>
+            <input type='password'></input>
           </div>
-
-          <div className='field'>
-            <input type='submit' value='Log in'></input>
-          </div>
-
-          </form>
-      </div>
-
-      <div className='foot_tainer'>
-        <div className='footer'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci a sapien imperdiet maximus. Suspendisse vel ex eget mauris efficitur finibus eu a enim. Aenean tellus neque, consectetur sit amet lacus a, hendrerit ornare urna. Fusce venenatis lorem vitae augue suscipit, </div>
-      </div>
-      </div>
+          <div className='field button'>
+              <input type='submit' value='Login'></input>
+            </div>
+        </form>
+      </section>
     </div>
+    <footer> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci a sapien imperdiet maximus.</footer>
+  </body>
   );
 }
 
