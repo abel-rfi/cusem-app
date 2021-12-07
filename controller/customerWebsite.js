dad2a408ee900b404a93332b9c667cd56350251b
abel-rfi <64470538+abel-rfi@users.noreply.github.com>
2021-12-07 13:11:20 +0700

    half done autoreply form

--- controller/customerWebsite.js
+++ controller/customerWebsite.js
@@ -1,6 +1,8 @@
 const auth = require('../middlewares/auth');
 const {fetchAll, fetchOne, create} = require('../middlewares/CRUD');
 const models = require('../models');
+const nodemailer = require("nodemailer");
+const { getMaxListeners } = require('../app');
 
 const User = models.User;
 
@@ -85,8 +87,58 @@ function form(req, res){
 		res.status(500).json({
 			message: "Something went wrong",
 			error: error
-		});		
+		});	
 	});
+	/*
+	async function main() {
+		let testAccount = await nodemailer.createTestAccount();
+		let transporter = nodemailer.createTransport({
+			host: "smtp.ethereal.email",
+			port: 587,
+			secure: false, // true for 465, false for other ports
+			auth: {
+			  user: testAccount.user, // generated ethereal user
+			  pass: testAccount.pass, // generated ethereal password
+			},
+		  });
+
+		let info = await transporter.sendMail({
+			from: '"Agent" <agent@example.com>',
+			to: req.body.email,
+			subject: "Thank you for your mail",
+			html: "<p>This is an automated reply mail. Our agent will reply back to you ASAP</p>"
+		})
+		console.log("Message sent: %s", info.messageId);
+  		// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  		// Preview only available when sending through an Ethereal account
+  		console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+	}
+	main().catch(console.error);
+	*/
+	async function main() {
+		let transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+			  user: "komputersainsteknik@gmail.com", 
+			  pass: "iuli2019", 
+			},
+		  });
+
+		let info = {
+			from: '"Agent" <agent@example.com>',
+			to: req.body.email,
+			subject: "Thank you for your mail",
+			html: "<p>This is an automated reply mail. Our agent will reply back to you ASAP</p>"
+		}
+
+		await transporter.sendMail(info);
+		
+		console.log("Message sent: %s", (await transporter.sendMail(info)).messageId);
+  		// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+	}
+	main().catch(console.error);
+	return res.redirect(`/customer-website`);
 }
 
 const render = async (req, res) => {
