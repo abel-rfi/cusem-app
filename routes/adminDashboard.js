2c9cf653e55f826b2feb4927ba73f0e9036f9809
Jibranwafi <71427316+Jibranwafi@users.noreply.github.com>
2021-12-14 12:10:49 +0700

    commit route

--- routes/adminDashboard.js
+++ routes/adminDashboard.js
@@ -7,6 +7,9 @@ router.get('/', adminController.renderDashb);
 router.get('/agent-manager', adminController.render);
 router.get('/agent-manager/agent-editor', adminController.renderAgent);
 router.get('/faq-list', adminController.renderFAQ); // from const admin controller, with the function render
+router.get('/report-list', adminController.renderReport);
+router.get('/customer-manager', adminController.renderCustomer);
+
 
 
 module.exports = router;
