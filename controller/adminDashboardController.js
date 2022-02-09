1cbd072e9343c343bf46a64ef5458a64123d8e0e
nashri <nashriaziz65@gmail.com>
2022-02-09 17:19:01 +0700

    admin user emplo list

---
+++ controller/adminDashboardController.js
@@ -0,0 +1,43 @@
+const Op = require('sequelize').Op;
+
+const models = require('../models');
+
+// Function Section
+
+// Render Section
+
+// Main Dashboard
+exports.render = async (req, res) => {
+	try {
+		return res.render('adminDashboard', {layout: 'adminNav'});
+	} catch(err) {
+		console.log(`msg: ${err.message}`);
+		// return res.redirect('/employee-login-page');
+		return res.status(500).json({msg: err.message});
+	}
+}
+// Main Dasboard End
+
+// User List
+exports.renderCL = async (req, res) => {
+	try {
+		return res.render('adminCustomerList', {layout: 'adminNav'})
+	} catch(err) {
+		console.log(`msg: ${err.message}`);
+		// return res.redirect('/employee-login-page');
+		return res.status(500).json({msg: err.message});
+	}
+}
+// User List End
+
+// Employee List
+exports.renderEL = async (req, res) => {
+	try {
+		return res.render('adminEmployeeList', {layout: 'adminNav'})
+	} catch(err) {
+		console.log(`msg: ${err.message}`);
+		// return res.redirect('/employee-login-page');
+		return res.status(500).json({msg: err.message});
+	}
+}
+// Employee List End
---
+++ routes/adminDashboardRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Controller = require('../controller/adminDashboardController');
+
+const router = express.Router();
+
+// Without Authorization
+router.get('/', Controller.render);
+router.get('/customer-list', Controller.renderCL);
+router.get('/employee-list', Controller.renderEL);
+// With Authorization
+
+module.exports = router
